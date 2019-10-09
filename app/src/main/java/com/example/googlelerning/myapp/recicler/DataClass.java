0df353b6845ec7756cb4feee1c5cbc95d18544d3
Vitas1968 <dor1968@mail.ru>
2019-10-09 19:55:16 +0700

    класс данных

--- app/src/main/java/com/example/googlelerning/myapp/recicler/DataClass.java
+++ app/src/main/java/com/example/googlelerning/myapp/recicler/DataClass.java
@@ -1,4 +1,15 @@
 package com.example.googlelerning.myapp.recicler;
 
+import android.graphics.drawable.Drawable;
+
 public class DataClass {
+
+    Drawable drawable;
+    String text;
+
+    DataClass(Drawable drawable, String text) {
+        this.drawable = drawable;
+        this.text = text;
+
+    }
 }
