c0f9027bcb15d64d69dc5ac096015456341c5455
Vitas1968 <dor1968@mail.ru>
2019-10-09 19:57:10 +0700

    переопределяем методы и добавляем внутренний класс ViewHolder

--- app/src/main/java/com/example/googlelerning/myapp/recicler/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/googlelerning/myapp/recicler/RecyclerViewAdapter.java
@@ -1,6 +1,43 @@
 package com.example.googlelerning.myapp.recicler;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter {
+import com.example.googlelerning.myapp.R;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        ImageView imageView;
+
+
+        ViewHolder(View view) {
+            super(view);
+            imageView = view.findViewById(R.id.imageView);
+            textView = view.findViewById(R.id.textView);
+        }
+
+    }
 }
