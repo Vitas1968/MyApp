30d21426ec27d885be0ba8e986aae66f9be30c4e
Vitas1968 <dor1968@mail.ru>
2019-10-14 14:08:42 +0700

    1 Замена Spinner на material.textfield.TextInputEditText 2 добавил material.snackbar.Snackbar выводит сообщение какой город выбран 3 добавлен слушатель к TextInputEditText 4 реализован метод выбора города из исходного массива

--- app/src/main/java/com/example/googlelerning/myapp/fragments/ListCityFragment.java
+++ app/src/main/java/com/example/googlelerning/myapp/fragments/ListCityFragment.java
@@ -7,94 +7,111 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.Spinner;
+
+import android.widget.TextView;
 import com.example.googlelerning.myapp.R;
 import com.example.googlelerning.myapp.SecondActivity;
-
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import java.util.Objects;
 
 public class ListCityFragment extends Fragment {
-    private static  final String LOG_TAG ="ListCityFragment";
-    private CheckBox tempCheckBox,windCheckBox,humCheckBox;
+    private static final String LOG_TAG = "ListCityFragment";
+    private CheckBox tempCheckBox, windCheckBox, humCheckBox;
     private Button viewWatherBtn;
-    private Spinner spinner;
-    private  int spinnerItemPosition;
-    private boolean isCheckedTempCheckBox,isCheckedWindCheckBox,isCheckedHumCheckBox;
+    private boolean isCheckedTempCheckBox, isCheckedWindCheckBox, isCheckedHumCheckBox;
+    private TextInputEditText inputCity;
     private OnFragmentInteractionListener mListener;
+    private Integer indexCity;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(LOG_TAG,"Выполнен onCreate()");
+        Log.d(LOG_TAG, "Выполнен onCreate()");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_list_city, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_city, container, false);
         initView(view);
         tempCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                isCheckedTempCheckBox=isChecked;
+                isCheckedTempCheckBox = isChecked;
             }
         });
 
         windCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                isCheckedWindCheckBox=isChecked;
+                isCheckedWindCheckBox = isChecked;
             }
         });
 
         humCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                isCheckedHumCheckBox=isChecked;
+                isCheckedHumCheckBox = isChecked;
             }
         });
 
+        inputCity.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)){
+                    TextView tv = (TextView) v;
+                    indexCity=checkCity(tv);
+                    return true;
+                }
+                return false;
+            }
+        });
         viewWatherBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.putExtra("spinnerItemPosition",spinnerItemPosition);
-                intent.putExtra("isCheckedHumCheckBox",isCheckedHumCheckBox);
-                intent.putExtra("isCheckedWindCheckBox",isCheckedWindCheckBox);
-                intent.putExtra("isCheckedTempCheckBox",isCheckedTempCheckBox);
+                Intent intent = new Intent();
+                //intent.putExtra("spinnerItemPosition", spinnerItemPosition);
+                intent.putExtra("city", indexCity);
+                intent.putExtra("isCheckedHumCheckBox", isCheckedHumCheckBox);
+                intent.putExtra("isCheckedWindCheckBox", isCheckedWindCheckBox);
+                intent.putExtra("isCheckedTempCheckBox", isCheckedTempCheckBox);
                 intent.setClass(Objects.requireNonNull(getActivity()), SecondActivity.class);
                 startActivity(intent);
             }
         });
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                spinnerItemPosition=position;
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
-        Log.d(LOG_TAG,"Выполнен onCreateView()");
+        Log.d(LOG_TAG, "Выполнен onCreateView()");
         return view;
     }
+
+    private Integer checkCity(TextView tv) {
+        String city = tv.getText().toString();
+        String[] arrayCity = getResources().getStringArray(R.array.cityList);
+        for (int i = 0; i < arrayCity.length; i++) {
+            if (city.equalsIgnoreCase(arrayCity[i])) {
+                Snackbar.make(tv, "Was chosen city "+arrayCity[i], Snackbar.LENGTH_LONG).show();
+                return i;
+            }
+        }
+        return -1;
+    }
+
 
     private void initView(View view){
         tempCheckBox=view.findViewById(R.id.tempCb);
         windCheckBox=view.findViewById(R.id.windCb);
         humCheckBox=view.findViewById(R.id.humidityCb);
         viewWatherBtn=view.findViewById(R.id.viewWatherBtn);
-        spinner=view.findViewById(R.id.spinner);
+        inputCity=view.findViewById(R.id.inputCity);
+
     }
 
 
@@ -163,7 +180,4 @@ public class ListCityFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Intent intent);
     }
-
-
-
 }
