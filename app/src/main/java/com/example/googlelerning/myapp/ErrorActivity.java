71ec7582defe30b82475f5bff08e4d20a1d77b33
Vitas1968 <dor1968@mail.ru>
2019-10-16 21:30:47 +0700

    то же

--- app/src/main/java/com/example/googlelerning/myapp/ErrorActivity.java
+++ app/src/main/java/com/example/googlelerning/myapp/ErrorActivity.java
@@ -2,7 +2,9 @@ package com.example.googlelerning.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ErrorActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class ErrorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error);
+        TextView errorTv = findViewById(R.id.errorTv);
+        Intent intent=getIntent();
+        errorTv.setText(intent.getStringExtra("notFound"));
     }
 }
